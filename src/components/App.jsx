1dc8a7e4a219f7449f7e229c48492f30f39cf986
MiaktynovS <ousm9595@gmail.com>
2022-12-24 19:42:06 +0200

    Create Profile and Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import Profile from "./Profile/Profile.jsx"
+import user from "./Profile/user.json"
+import Statistics from "./Statistics/Statistics.jsx";
+import data from "./Statistics/data.json"
+
+
+
+export const App = () => (
+    <>
+    <Profile
+    name = {user.name}
+    tag = {user.tag}
+    location = {user.location}
+    avatar = {user.avatar}
+    stats = {user.stats}
+    />
+    <Statistics title="Upload stats" stats={data} />
+    </>
+)
+
+export default App;
+
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
